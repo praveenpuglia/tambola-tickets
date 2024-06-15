af991e0e091348105273994d5c4b244949f54c50
Praveen Puglia <praveen.puglia@smallcase.com>
2024-06-15 11:24:50 +0530

    build: fix the deployment public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/tambola-tickets/' : '/',
+  publicPath: '/',
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
