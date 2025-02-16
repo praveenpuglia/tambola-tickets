b9a7712afb98bf06a548333e2fa3c66c4dd74633
Praveen Puglia <praveen.puglia@smallcase.com>
2025-02-16 11:21:03 +0530

    chore: cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es2022: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "babel-eslint"
+    ecmaVersion: 2022,
+    sourceType: 'module'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 };
